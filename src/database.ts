d8c1ca9e53d12078d0eceda6950075a0cbad1745
oldrover <oldrover@web.de>
2021-06-03 13:10:08 +0200

    modified database.ts for dev/test db switching

--- src/database.ts
+++ src/database.ts
@@ -3,18 +3,34 @@ import { Pool } from 'pg';
 
 dotenv.config()
 
+let client;
+
 const {
     POSTGRES_HOST,
     POSTGRES_DB,
+    POSTGRES_TEST_DB,
     POSTGRES_USER,
-    POSTGRES_PASSWORD
+    POSTGRES_PASSWORD,
+    ENV
 } = process.env;
 
-const client = new Pool ({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-});
+if(ENV == 'test') {
+    const client = new Pool ({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    });
+}
+
+if(ENV == 'test') {
+    const client = new Pool ({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    });
+}
+
 
 export default client;
