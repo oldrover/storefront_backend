35522feeba7a8145ebd00c32aca213677e1c12bd
oldrover <oldrover@web.de>
2021-06-03 13:10:36 +0200

    completed migration files for users table

--- migrations/sqls/20210601131519-users-table-up.sql
+++ migrations/sqls/20210601131519-users-table-up.sql
@@ -1,1 +1,6 @@
-/* Replace with your SQL commands */
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    password VARCHAR(256) 
+);
