e739197afa25d50b14bec0e3abe092f880eb1655
oldrover <oldrover@web.de>
2021-06-05 14:03:20 +0200

    added products routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response} from 'express';
 import user_routes from './controllers/users';
+import product_routes from './controllers/products';
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
 
-app.use(express.json);
+app.use(express.json());
 
 app.get('/', function (_req: Request, res: Response) {
     res.send('Server is running!');
 });
 
 user_routes(app);
+product_routes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`);
