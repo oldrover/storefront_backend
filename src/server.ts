3e6809b5f210d181f9f7c07884e118c844616a60
oldrover <oldrover@web.de>
2021-06-05 14:29:11 +0200

    added order routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response} from 'express';
 import user_routes from './controllers/users';
 import product_routes from './controllers/products';
+import order_routes from './controllers/orders';
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
@@ -13,6 +14,7 @@ app.get('/', function (_req: Request, res: Response) {
 
 user_routes(app);
 product_routes(app);
+order_routes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`);
