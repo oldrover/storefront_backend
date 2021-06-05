ceab21a744e0f0ac45ccf1041115573b8af412b9
oldrover <oldrover@web.de>
2021-06-05 12:59:21 +0200

    added user route to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
+import user_routes from './controllers/users';
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
 
-app.use(express.json());
+app.use(express.json);
 
 app.get('/', function (_req: Request, res: Response) {
-    res.send('Hello World!');
+    res.send('Server is running!');
 });
+
+user_routes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`);
