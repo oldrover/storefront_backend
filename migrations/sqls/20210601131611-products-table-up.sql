6399109c93b845d469baf834ea8297933224b2de
oldrover <oldrover@web.de>
2021-06-07 12:09:15 +0200

    corrected col names in sql files

--- migrations/sqls/20210601131611-products-table-up.sql
+++ migrations/sqls/20210601131611-products-table-up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    price NUMERIC(5,2) NOT NULL
+    price NUMERIC(5,2) NOT NULL,
+    category VARCHAR NOT NULL
 );
