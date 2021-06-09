02d499f24a2304edaa807249706d7048e5877691
oldrover <oldrover@web.de>
2021-06-09 13:47:25 +0200

    double quoted names

--- migrations/sqls/20210601131519-users-table-up.sql
+++ migrations/sqls/20210601131519-users-table-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    userName VARCHAR(50) NOT NULL,
-    firstName VARCHAR(100),
-    lastName VARCHAR(100),
+    "userName" VARCHAR(50) NOT NULL,
+    "firstName" VARCHAR(100),
+    "lastName" VARCHAR(100),
     password VARCHAR(256) NOT NULL 
 );
--- migrations/sqls/20210603115422-order-products-table-up.sql
+++ migrations/sqls/20210603115422-order-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products (
     id SERIAL PRIMARY KEY,
     quantity INTEGER,
-    orderId BIGINT REFERENCES orders(id),
-    productId BIGINT REFERENCES products(id)
+    "orderId" BIGINT REFERENCES orders(id),
+    "productId" BIGINT REFERENCES products(id)
 );
