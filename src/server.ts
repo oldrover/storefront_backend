9096e8266a1e02579c077e7948702a6cf660bb3b
oldrover <oldrover@web.de>
2021-06-09 09:33:20 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response} from 'express';
-import user_routes from './controllers/users';
-import product_routes from './controllers/products';
-import order_routes from './controllers/orders';
+import cors from 'cors';
+import user_routes from './helpers/users';
+import product_routes from './helpers/products';
+import order_routes from './helpers/orders';
 
-const app: express.Application = express();
+const app = express();
 const address: string = "0.0.0.0:3000";
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', function (_req: Request, res: Response) {
@@ -19,3 +21,4 @@ order_routes(app);
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`);
 });
+export default app;
